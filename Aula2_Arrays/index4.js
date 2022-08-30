6a3a2acdee061c6fcd3a3324f60fe4da225582f9
Camandaroba06 <cristianogregorymc@gmail.com>
2022-08-30 15:55:46 -0300

    1 file modified

--- Aula2_Arrays/index4.js
+++ Aula2_Arrays/index4.js
@@ -11,3 +11,27 @@ const redesSociais = [
 // Forma de saber o indice de um elemento:
 const indic_Tiktok = redesSociais.indexOf('Tiktok');
 console.log(indic_Tiktok);
+
+// Forma de saber se determinado item está no Array:
+console.log(redesSociais.includes('Tumblr')); // False
+console.log(redesSociais.includes('Youtube')); // True
+
+// Percorrendo Arrays:
+for (let i = 0; i < redesSociais.length; i++) {
+  console.log(redesSociais[i]);
+}
+for (let i = 0; i < redesSociais.length; i++) {
+  // Ou de uma forma mais bacana:
+  console.log(`${i}: ${redesSociais[i]}`);
+}
+for (const i in redesSociais) {
+  console.log(redesSociais[i]);
+}
+for (const valor of redesSociais) {
+  console.log(valor);
+}
+redesSociais.forEach((valor, indice, redes) => {
+  console.log(valor);
+  console.log(indice);
+  console.log(redes);
+});
