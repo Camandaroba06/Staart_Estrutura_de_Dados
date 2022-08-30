ce2d910c4fe1fe2875b5bc3cef38366e44570165
Camandaroba06 <cristianogregorymc@gmail.com>
2022-08-29 22:55:17 -0300

    1 file added

---
+++ Aula2_Arrays/index3.js
@@ -0,0 +1,7 @@
+let meuArray1 = ['Camandaroba', 'Vanessa', 'Jorge'];
+let meuArray2 = new Array();
+let meuArray3 = new Array('Ricardo');
+
+console.log(meuArray1);
+console.log(meuArray2);
+console.log(meuArray3);
