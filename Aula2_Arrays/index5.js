8960b00e413ca487380adc3513f9bdbaa3be92ff
Camandaroba06 <cristianogregorymc@gmail.com>
2022-08-30 16:30:11 -0300

    1 file modified

--- Aula2_Arrays/index5.js
+++ Aula2_Arrays/index5.js
@@ -1,5 +1,18 @@
+// Map:
+
 const aPagarEmReais = [10, 20, 40, 100];
 const cotacaoDolar = 5;
-const aPagarEmDolar = aPagarEmReais.map((valor) => valor * 5);
+const aPagarEmDolar = aPagarEmReais.map((valor) => valor * cotacaoDolar);
 
 console.table(aPagarEmDolar);
+
+// Filter:
+
+const contas = [10, 20, 40, 100];
+const caras = contas.filter((valor) => valor > 30);
+console.table(caras);
+
+// Find:
+//const contas = [10, 20, 40, 100];
+const caras_find = contas.find((valor) => valor > 30); // encontra a primeira maior que 30 e armazena
+console.table(caras_find);
