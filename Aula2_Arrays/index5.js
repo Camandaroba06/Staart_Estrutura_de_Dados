9fe10caafd11b7d8a59a1b17a7e371741f99f01c
Camandaroba06 <cristianogregorymc@gmail.com>
2022-08-30 16:34:34 -0300

    1 file modified

--- Aula2_Arrays/index5.js
+++ Aula2_Arrays/index5.js
@@ -14,5 +14,12 @@ console.table(caras);
 
 // Find:
 //const contas = [10, 20, 40, 100];
-const caras_find = contas.find((valor) => valor > 30); // encontra a primeira maior que 30 e armazena
-console.table(caras_find);
+const caras_Find = contas.find((valor) => valor > 30); // encontra a primeira maior que 30 e armazena
+const caras_Find_Ind = contas.findIndex((valor) => valor > 30); //encontra o indice
+console.table(caras_Find);
+console.log(caras_Find_Ind);
+
+// Reduce:
+//const contas = [10, 20, 40, 100];
+const totalAPagar = contas.reduce((valor, acumulador) => valor + acumulador, 0);
+console.log(totalaPagar);
