14c8b157657c7339f7c48e717bc1ab8d94a902e7
Camandaroba06 <cristianogregorymc@gmail.com>
2022-08-30 15:45:21 -0300

    1 file modified

---
+++ Aula2_Arrays/index4.js
@@ -0,0 +1,13 @@
+const redesSociais = [
+  'Instagram',
+  'Youtube',
+  'Facebook',
+  'Tiktok',
+  'Twitter',
+  'Linkedin',
+  'Pinterest',
+];
+
+// Forma de saber o indice de um elemento:
+const indic_Tiktok = redesSociais.indexOf('Tiktok');
+console.log(indic_Tiktok);
