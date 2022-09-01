e2a5716a6cb609e62c28249e4023b4f78fb555fc
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-01 19:12:37 -0300

    1 file modified

--- Aula2_Arrays/index6.js
+++ Aula2_Arrays/index6.js
@@ -4,5 +4,8 @@ const Programador = {
   nascimento: '04/06/2003',
   Universidade: 'UFJF',
   grupoFavorito: 'PET Elétrica UFJF',
+  Linguagens_Favoritas: ['Python', 'Javascript'],
 };
 console.table(Programador);
+console.log(Programador.nome);
+console.log(Programador.grupoFavorito);
