7b1c8bc089746485cd288bf0ce427f9c7d33e116
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-01 19:10:40 -0300

    1 file added

---
+++ Aula2_Arrays/index6.js
@@ -0,0 +1,8 @@
+// Objetos e JSON
+const Programador = {
+  nome: 'Camandaroba',
+  nascimento: '04/06/2003',
+  Universidade: 'UFJF',
+  grupoFavorito: 'PET Elétrica UFJF',
+};
+console.table(Programador);
