8801a13d978c7f04a19a4126a9242315ad0bb8c6
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-01 19:17:44 -0300

    1 file modified

--- Aula2_Arrays/index6.js
+++ Aula2_Arrays/index6.js
@@ -9,3 +9,21 @@ const Programador = {
 console.table(Programador);
 console.log(Programador.nome);
 console.log(Programador.grupoFavorito);
+
+//Classes
+
+class Programador {
+  constructor(
+    nome,
+    nascimento,
+    Universidade,
+    grupoFavorito,
+    Linguagens_Favoritas = []
+  ) {
+    this.nome = nome;
+    this.nascimento = nascimento;
+    this.Universidade = Universidade;
+    this.grupoFavorito = grupoFavorito;
+    this.Linguagens_Favoritas = Linguagens_Favoritas;
+  }
+}
