c3c5a1859fb6e58bd049a764b041348481a21c62
Camandaroba06 <88840308+Camandaroba06@users.noreply.github.com>
2022-09-03 10:51:14 -0300

    1 file modified

--- Aula2_Arrays/index7.js
+++ Aula2_Arrays/index7.js
@@ -4,4 +4,5 @@
             - Elas seguem o principio de LIFO(Last In, First Out)
             - Nela só temos acesso a um único item, que é o último a ter sido colocado na pilha
             - O final é conhecido como TOPO
+            - Ex.: Botão voltar do navegador e ctrl+z
 */
