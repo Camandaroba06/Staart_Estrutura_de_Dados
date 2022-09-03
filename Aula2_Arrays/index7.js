29c838f0ccabc338af5d6c4e94a1e836e18a3f14
Camandaroba06 <88840308+Camandaroba06@users.noreply.github.com>
2022-09-03 10:38:49 -0300

    1 file modified

--- Aula2_Arrays/index7.js
+++ Aula2_Arrays/index7.js
@@ -3,5 +3,5 @@
       -> Pilhas:
             - Elas seguem o principio de LIFO(Last In, First Out)
             - Nela só temos acesso a um único item, que é o último a ter sido colocado na pilha
-            
+            - O final é conhecido como TOPO
 */
