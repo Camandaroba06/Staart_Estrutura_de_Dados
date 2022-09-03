b95ff21147ee65710553668ea2483b17258386de
Camandaroba06 <88840308+Camandaroba06@users.noreply.github.com>
2022-09-03 10:38:04 -0300

    1 file modified

--- Aula2_Arrays/index7.js
+++ Aula2_Arrays/index7.js
@@ -2,5 +2,6 @@
     Aprendendo sobre Estrutura de Dados:
       -> Pilhas:
             - Elas seguem o principio de LIFO(Last In, First Out)
-
+            - Nela só temos acesso a um único item, que é o último a ter sido colocado na pilha
+            
 */
