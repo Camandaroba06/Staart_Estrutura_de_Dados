db099b9c8e091d6c6d9b83a578e97278baa7bec6
Camandaroba06 <88840308+Camandaroba06@users.noreply.github.com>
2022-09-03 22:43:45 -0300

    1 file added

---
+++ Aula2_Arrays/index10.js
@@ -0,0 +1,12 @@
+const meuDicionario = new Map();
+meuDicionario.set('oi', 'Oi tudo bem?');
+meuDicionario.set('blz', 'beleza');
+meuDicionario.set('vc', 'Você');
+console.table(meuDicionario);
+console.log(meuDicionario.get('vc'));
+
+const indiceContatos = [
+  ['Gabriel', '98821235'],
+  ['Jorge', '984822352'],
+  ['Alice', '983221235'],
+];
